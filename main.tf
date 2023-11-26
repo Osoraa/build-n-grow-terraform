c7f51fafada9aa32a894c1c8acbb5327a9c0ecd9
devn0n <osoraanwankwo@gmail.com>
2023-11-26 10:10:45 +0100

    Deploy subnet

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
-resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/24"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+resource "aws_vpc" "bng_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        name = "build-n-grow"
-    }
+  tags = {
+    Name = "bng_vpc"
+    env  = "dev"
+  }
+}
+
+resource "aws_subnet" "bng_public_subnet" {
+  vpc_id                  = aws_vpc.bng_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "bng_public_subnet"
+    env  = "dev"
+  }
 }
